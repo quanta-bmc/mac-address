c21c6f140f07eff689c51bb8ba1519051879e07e
Scron Chang <scron.chang@quanta.corp-partner.google.com>
2020-11-26 19:42:12 +0800

    Add find-fru-bus.sh

---
+++ find-fru-bus.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+confPath=/usr/share/mac-address
+confFilename=config.txt
+
+#check config file
+if [ ! -f "${confPath}/${confFilename}" ]; then
+    exit 0
+fi
+
+tmp=$(grep "eeprom=" ${confPath}/${confFilename})
+if [ -z "${tmp}" ]; then
+    exit 0
+fi
+
+targetName="${tmp#*eeprom=}"
+targetPath=$(grep -xl "${targetName}" /sys/bus/i2c/devices/*/of_node/name)
+
+# Get target i2c number
+if [ -n "${targetPath}" ]; then
+    targetI2cNo=${targetPath%-00*}
+    targetI2cNo=${targetI2cNo#*devices/}
+
+    # Modify config.txt
+    if [ -n "${targetI2cNo}" ]; then
+        sed -i -e 's/fruBusNum=.*/fruBusNum='"${targetI2cNo}"'/g' ${confPath}/${confFilename}
+    fi
+fi
